88241eaa978846acd855277c4dfc8c6657306873
natalyansharapova <natalya.n.sharapova@gmail.com>
2021-10-24 17:33:18 +0400

    methods commit

--- main.py
+++ main.py
@@ -3,9 +3,31 @@
 # obj = json.loads(json_text)
 # print(obj["messages"][1])
 
-
-
 import requests
-response = requests.post("https://playground.learnqa.ru/api/long_redirect")
+# Запрос без параметра
+response = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type")
 print(response.url)
-print(len(response.history))
+print(response.status_code)
+
+# Запрос не из списка
+response = requests.head("https://playground.learnqa.ru/ajax/api/compare_query_type")
+print(response.url)
+print(response.status_code)
+
+# Запрос с правильным значением метод
+response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", params={"method":"GET"})
+print(response.url)
+print(response.status_code)
+print(response.text)
+
+#
+methods = ["POST","GET", "DELETE", "PUT"]
+for i in methods:
+    for j in methods:
+      if i =="POST":
+        response = requests.request(i,"https://playground.learnqa.ru/ajax/api/compare_query_type", params={"method":j})
+        print(i, j, response.url,  response.status_code, response.text)
+      else:
+        response = requests.request(i, "https://playground.learnqa.ru/ajax/api/compare_query_type",
+                               data={"method": j})
+        print(i, j, response.url,  response.status_code, response.text)
