6c6a1d6114b3773a700490b9317f0f56202167f3
natalyansharapova <natalya.n.sharapova@gmail.com>
2021-10-24 18:54:03 +0400

    token commit

--- main.py
+++ main.py
@@ -1,33 +1,17 @@
-# import json
-# json_text = '{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}'
-# obj = json.loads(json_text)
-# print(obj["messages"][1])
-
 import requests
-# Запрос без параметра
-response = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type")
-print(response.url)
-print(response.status_code)
+import json
+import time
 
-# Запрос не из списка
-response = requests.head("https://playground.learnqa.ru/ajax/api/compare_query_type")
-print(response.url)
-print(response.status_code)
-
-# Запрос с правильным значением метод
-response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", params={"method":"GET"})
-print(response.url)
-print(response.status_code)
-print(response.text)
-
-#
-methods = ["POST","GET", "DELETE", "PUT"]
-for i in methods:
-    for j in methods:
-      if i =="POST":
-        response = requests.request(i,"https://playground.learnqa.ru/ajax/api/compare_query_type", params={"method":j})
-        print(i, j, response.url,  response.status_code, response.text)
-      else:
-        response = requests.request(i, "https://playground.learnqa.ru/ajax/api/compare_query_type",
-                               data={"method": j})
-        print(i, j, response.url,  response.status_code, response.text)
+response = requests.get("https://playground.learnqa.ru/ajax/api/longtime_job")
+obj = json.loads(response.text)
+token = obj["token"]
+time_left = obj["seconds"]
+response = requests.get("https://playground.learnqa.ru/ajax/api/longtime_job", params={"token":token})
+obj1 = json.loads(response.text)
+if obj1["status"] == 'Job is NOT ready' :
+   print("Status OK -",obj1["status"])
+time.sleep(time_left)
+response = requests.get("https://playground.learnqa.ru/ajax/api/longtime_job", params={"token":token})
+obj1 = json.loads(response.text)
+if obj1["status"] == 'Job is ready' :
+   print("Status OK -",obj1["status"])
