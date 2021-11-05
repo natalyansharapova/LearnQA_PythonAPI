01b306320a69ff9a431462e703decd8443bd374c
natalyansharapova <natalya.n.sharapova@gmail.com>
2021-11-05 10:11:12 +0400

    framework commit

---
+++ .pytest_cache/api_test_auth_negative.py
@@ -0,0 +1,21 @@
+import requests
+
+class TestUserAuth:
+    def test_auth_user(self):
+        data = {
+            'email': 'vinkotov@example.com',
+            'password' : '1234'
+        }
+
+        response1 = requests.post("https://playground.learnqa.ru/api/user/login", data=data)
+        assert "auth_sid" in response1.cookies, "There is no auth in the response"
+        assert "x-csrf-token" in response1.headers, "There is not token header in the response"
+        assert "user_id" in response1.json(), "There is no user in the response"
+        auth_sid=response1.cookies.get("auth_sid")
+        token = response1.headers.get("x-csrf-token")
+        user_id_from_auth_method = response1.json()["user_id"]
+        response2 = requests.get(
+            "https://playground.learnqa.ru/api/user/auth",
+             headers={"x-csrf-token":token},
+             cookies={"auth_sid":auth_sid},
+        )
